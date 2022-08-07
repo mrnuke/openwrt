658858ae634b5ec8f415b607e922f7f320983cea
Alexandru Gagniuc <mr.nuke.me@gmail.com>
2022-08-07 10:15:35 -0500

    [WIP] ipq60xx: add TP-Link EAP610-Outdoor support

--- target/linux/ipq60xx/image/generic.mk
+++ target/linux/ipq60xx/image/generic.mk
@@ -30,3 +30,14 @@ define Device/cp01-c1
 	SOC := ipq6018
 endef
 TARGET_DEVICES += cp01-c1
+
+define Device/eap610-outdoor
+	$(call Device/FitImage)
+	$(call Device/UbiFit)
+	DEVICE_VENDOR := TP-Link
+	DEVICE_MODEL := EAP610-Outdoor
+	BLOCKSIZE := 128k
+	PAGESIZE := 2048
+	SOC := ipq6018
+endef
+TARGET_DEVICES += eap610-outdoor
