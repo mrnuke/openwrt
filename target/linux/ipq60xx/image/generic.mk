e38e42483cddc6866208f5ebe6444e97c53d2810
Alexandru Gagniuc <mr.nuke.me@gmail.com>
2022-09-24 15:06:34 -0500

    [WIP] ipq60xx: add support for Linksys MR7350

--- target/linux/ipq60xx/image/generic.mk
+++ target/linux/ipq60xx/image/generic.mk
@@ -41,3 +41,16 @@ define Device/eap610-outdoor
 	SOC := ipq6018
 endef
 TARGET_DEVICES += eap610-outdoor
+
+define Device/linksys_mr7350
+	$(call Device/FitImage)
+	$(call Device/UbiFit)
+	DEVICE_VENDOR := Linksys
+	DEVICE_MODEL := MR7350
+	BLOCKSIZE := 128k
+	PAGESIZE := 2048
+	SOC := ipq6018
+	DEVICE_PACKAGES := kmod-leds-pca963x \
+		kmod-usb3 kmod-usb-dwc3 kmod-usb-dwc3-qcom
+endef
+TARGET_DEVICES += linksys_mr7350
