63816e89563c69a69b78ec70e0d626a31ab44cd7
Alexandru Gagniuc <mr.nuke.me@gmail.com>
2022-10-23 20:34:46 -0500

    ipq6018: MR7350: Use correct device name (for sysupgrade)

--- target/linux/ipq60xx/image/generic.mk
+++ target/linux/ipq60xx/image/generic.mk
@@ -43,11 +43,11 @@ define Device/eap610-outdoor
 endef
 TARGET_DEVICES += eap610-outdoor
 
-define Device/mr7350
+define Device/linksys_mr7350
 	$(call Device/FitImage)
 	DEVICE_VENDOR := Linksys
 	DEVICE_MODEL := MR7350
 	SOC := ipq6018
 	DEVICE_PACKAGES := kmod-leds-pca963x
 endef
-TARGET_DEVICES += mr7350
+TARGET_DEVICES += linksys_mr7350
