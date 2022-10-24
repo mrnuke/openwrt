a2beb4f3b729da71b97dc51bc8f432dfcadea801
Alexandru Gagniuc <mr.nuke.me@gmail.com>
2022-10-23 20:35:11 -0500

    [WTF] ipq6018: MR7350: Build a UBI FIT image

--- target/linux/ipq60xx/image/generic.mk
+++ target/linux/ipq60xx/image/generic.mk
@@ -45,8 +45,11 @@ TARGET_DEVICES += eap610-outdoor
 
 define Device/linksys_mr7350
 	$(call Device/FitImage)
+	$(call Device/UbiFit)
 	DEVICE_VENDOR := Linksys
 	DEVICE_MODEL := MR7350
+	BLOCKSIZE := 128k
+	PAGESIZE := 2048
 	SOC := ipq6018
 	DEVICE_PACKAGES := kmod-leds-pca963x
 endef
