20bfcdbdaf7bedba10e7ca9f1018d271c9f6bc70
Alexandru Gagniuc <mr.nuke.me@gmail.com>
2025-05-02 23:00:54 -0500

    [WIP] qualcommbe: add TP-Link Archer BE550

--- target/linux/qualcommbe/image/ipq95xx.mk
+++ target/linux/qualcommbe/image/ipq95xx.mk
@@ -12,3 +12,18 @@ define Device/qcom_rdp433
 	IMAGE/sysupgrade.bin := append-kernel | pad-to 64k | append-rootfs | pad-rootfs | check-size | append-metadata
 endef
 TARGET_DEVICES += qcom_rdp433
+
+define Device/tplink_archer-be550-v1
+	$(call Device/FitImageLzma)
+	DEVICE_VENDOR := TP-Link
+	DEVICE_MODEL := Archer BE550 v1
+# 	DEVICE_VARIANT := AP-AL02-C4
+# 	BOARD_NAME := ap-al02.1-c4
+# 	DEVICE_DTS_CONFIG := config@rdp433
+	SOC := ipq9554
+# 	KERNEL_INSTALL := 1
+# 	KERNEL_SIZE := 6096k
+# 	IMAGE_SIZE := 25344k
+	IMAGE/sysupgrade.bin := append-kernel | pad-to 64k | append-rootfs | pad-rootfs | check-size | append-metadata
+endef
+TARGET_DEVICES += tplink_archer-be550-v1
